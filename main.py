f45a8c88b7290bdf8f729a31f37c1b9ebd335ce3
johnny c <56122426+pearmypie@users.noreply.github.com>
2023-04-22 12:12:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+import ctypes
+
+from sys import platform
 from classes import City, Settings
 from functions import setup_dataframe, setup_cities, setup_projection, pygame_loop 
 
 
 def main():
+    #check for OS
+    if platform == "win32":
+        ctypes.windll.user32.SetProcessDPIAware()
+
     # setup the settings 
     bg_color = (0, 0, 0)
     fg_color = (255, 255, 255)
